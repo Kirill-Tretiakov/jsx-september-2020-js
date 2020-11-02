3826f652ad9deab00817ab70f52ba1109a5591bd
Kirill-Tretiakov <kirill.tretyakoff@gmail.com>
2020-11-02 13:01:47 +0200

    My 1 js homework

---
+++ homework1/script.js
@@ -0,0 +1,492 @@
+// Task #1
+
+// let a = 'hello';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+// let e = 1;
+// let f = -999;
+// let k = 123;
+// let g = 3.14;
+// let z = 2.7;
+// let q = 16;
+// let n = true;
+// let ab = false;
+//
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(k);
+// console.log(g);
+// console.log(z);
+// console.log(q);
+// console.log(n);
+// console.log(ab);
+//
+// document.write(a);
+// document.write(b);
+// document.write(c);
+// document.write(d);
+// document.write(e);
+// document.write(f);
+// document.write(k);
+// document.write(g);
+// document.write(z);
+// document.write(q);
+// document.write(n);
+// document.write(ab);
+//
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(k);
+// alert(g);
+// alert(z);
+// alert(q);
+// alert(n);
+// alert(ab);
+
+
+// Task #2
+
+// let a = 'kirill';
+// let b = 'lera';
+// let c = 'artem';
+// let d = 'marina';
+// let e = 19;
+// let f = 20;
+// let k = 25;
+// let g = 30;
+// let z = 2.01;
+// let q = 123.213;
+// let n = false;
+// let ab = true;
+//
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(k);
+// console.log(g);
+// console.log(z);
+// console.log(q);
+// console.log(n);
+// console.log(ab);
+//
+// document.write(a);
+// document.write(b);
+// document.write(c);
+// document.write(d);
+// document.write(e);
+// document.write(f);
+// document.write(k);
+// document.write(g);
+// document.write(z);
+// document.write(q);
+// document.write(n);
+// document.write(ab);
+//
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(k);
+// alert(g);
+// alert(z);
+// alert(q);
+// alert(n);
+// alert(ab);
+
+// Task #3
+
+//
+// const name = 'kirill';
+// const name1 = 'lera';
+// const name2 = 'artem';
+//
+// const age = 19;
+// const age1 = 18;
+// const age2 = 17;
+//
+// console.log(name);
+// console.log(name1);
+// console.log(name2);
+//
+// document.write(name);
+// document.write(name1);
+// document.write(name2);
+//
+// alert(name);
+// alert(name1);
+// alert(name2);
+//
+// console.log(age);
+// console.log(age1);
+// console.log(age2);
+//
+// document.write(age);
+// document.write(age1);
+// document.write(age2);
+//
+// alert(age);
+// alert(age1);
+// alert(age2);
+
+// Task #4
+
+// let name = prompt('Enter your name');
+// let middleName = prompt('Enter your middle name');
+// let surname = prompt('Enter your surname');
+//
+// console.log(name);
+// console.log(surname);
+// console.log(middleName);
+//
+// document.write(name);
+// document.write(middleName);
+// document.write(surname);
+//
+// alert(name);
+// alert(middleName);
+// alert(surname);
+
+// Task #5
+
+// let person = prompt('Enter your name:')
+//     ,middleName = prompt('Enter your middlename')
+//     ,surname = prompt('Enter your surname');
+//
+// console.log(name);
+// console.log(surname);
+// console.log(middleName);
+//
+// document.write(name);
+// document.write(middleName);
+// document.write(surname);
+//
+// alert(name);
+// alert(middleName);
+// alert(surname);
+
+// Task #6
+
+// let person = prompt('Enter your name:')
+//     ,middleName = prompt('Enter your middlename')
+//     ,surname = prompt('Enter your surname');
+//
+// console.log(name);
+// console.log(surname);
+// console.log(middleName);
+//
+// document.write(name);
+// document.write(middleName);
+// document.write(surname);
+//
+// alert(name);
+// alert(middleName);
+// alert(surname);
+
+// Task #7
+
+// let number1 = +prompt('Enter number1:');
+// let number2 = +prompt('Enter number2:');
+// let number3 = +prompt('Enter number3:');
+//
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+
+// Task #8
+
+// let number1 = parseInt(prompt('Enter number1:'));
+// let number2 = parseInt(prompt('Enter number2:'));
+// let number3 = parseInt(prompt('Enter number3:'));
+// let number4 = parseInt(prompt('Enter number4:'));
+//
+// let result = number1 + number2 + number3 + number4;
+//
+// console.log(result);
+
+// Task #9
+
+// let number1 = parseFloat(prompt('Enter number1:'));
+// let number2 = parseFloat(prompt('Enter number2:'));
+// let number3 = parseFloat(prompt('Enter number3:'));
+//
+//
+// let result = number1 + number2 + number3;
+//
+// console.log(result);
+
+// Task #10
+
+// let number1 = Math.round(prompt('Enter number1:'));
+// let number2 = Math.round(prompt('Enter number2:'));
+// let number3 = Math.round(prompt('Enter number3:'));
+//
+//
+// let result = number1 + number2 + number3;
+//
+// console.log(result);
+
+
+// Task #11
+
+// let number1 = Math.round(prompt('Enter number1:'));
+// let number2 = Math.round(prompt('Enter number2:'));
+//
+//
+// let result = Math.pow(number1, number2)
+//
+// console.log(result);
+//
+
+// Task #12
+//
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+//
+// console.log(typeof (a));
+// console.log(typeof (b));
+// console.log(typeof (c));
+// console.log(typeof (d));
+//
+
+// Task #13
+
+// let a = 'number';
+// let b = 'string';
+// let c = 'boolean';
+// let d = 'undefined';
+//
+// console.log(typeof (a));
+// console.log(typeof (b));
+// console.log(typeof (c));
+// console.log(typeof (d));
+
+
+// Task #14?
+
+// let a = 'number';
+// let b = 'string';
+// let c = 'boolean';
+// let d = 'undefined';
+//
+// console.log(typeof (a));
+// console.log(typeof (b));
+// console.log(typeof (c));
+// console.log(typeof (d));
+
+// Task #15
+
+// let a = 'number';
+// let b = 'string';
+// let c = 'boolean';
+// let d = 'undefined';
+//
+// console.log(typeof (a));
+// console.log(typeof (b));
+// console.log(typeof (c));
+// console.log(typeof (d));
+
+// CLASS
+
+// Task #1
+
+// let str = 'Hello!';
+// let num = 123;
+// let flag = true;
+// let txt = "true";
+//
+// console.log(typeof (str));
+// console.log(typeof (num));
+// console.log(typeof (flag));
+// console.log(typeof (txt));
+
+
+// Task #2
+
+// let  a1 = 5+3;
+// let  a2 = 5-3;
+// let  a3 = 5*3;
+// let  a4 = 5/3;
+// let  a5 = 5%3;
+//
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+
+// Task #3
+
+// let  a6 = 5%3;
+// let  a7 = 3%5;
+// let  a8 = 5*'3';
+// let  a9 = '5'-3;
+// let  a10 = 75 + 'kg';
+//
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+
+
+// Task #4
+
+// let height = 23 ;
+// let width = 10;
+//
+// let s = 23*10;
+//
+// console.log(`Площадь прямоугольника равняеться : ${s}`);
+
+// Task #5
+
+// let heightC =  10;
+// let dC = 4;
+//
+// let v = heightC*dC;
+//
+// console.log(v);
+
+// Task #6
+
+// let n = 3;
+// let m = 4;
+//
+// let k = Math.pow(3,2) + Math.pow(4,2);
+//
+// console.log(k);
+
+// Task #7
+
+// let str = 'Hello world';
+//
+// document.write(str);
+// console.log(str);
+// alert(str);
+
+// Task #8
+
+// let name = 'Kirill';
+// let age = 19;
+// let hobby = 'Box';
+//
+// alert(`${name}\n ${age}\n ${hobby}`);
+
+// Task #9
+
+// let str1 = 'Кто';
+// let str2 = 'ты';
+// let str3 = 'такой?';
+//
+// let concatenation = str+str2+str3;
+// document.write(concatenation);
+
+// Task #10
+
+// let str = "20";
+// let a = 5;
+//
+// document.write(str +a + "<br/>")//result 205
+// document.write(str -a + "<br/>")//result 15
+// document.write(str*"2" + "<br/>")//result 40
+// document.write(str/2 + "<br/>")///result10
+
+// Task #11
+
+// console.log(parseInt('3.14'));//result 3
+// console.log(parseInt('-7.875'));//result -7
+// console.log(parseInt('435'));//result 435
+// console.log(parseInt('Вася'));//result NaN
+
+// Task #12
+
+// let str = prompt('Enter something' , 'ho-ho');
+// console.log(str)
+
+// Task #13
+
+// let a = +prompt("Enter first number");
+// let b = +prompt("Enter second number");
+// let result = a+b
+// alert(result);
+
+// Task #14
+
+// let name = prompt("Enter your name:");
+// let surname = prompt("Enter your surname ");
+// let age = prompt("Enter your age");
+//
+// let message = `Доброго вечора ${name} ${surname}, мои поздравления , что вам ${age} !`;
+// alert(message)
+
+// DODATKOVE!
+
+// Task #1
+
+// let a = +prompt("Enter first number");
+// let b = +prompt("Enter the s econd number");
+// let c = +prompt("Entert the third number");
+//
+// if (a<b && a<c && b<c ){
+//     console.log(a);
+//     console.log(b)
+//     console.log(c)
+// }
+
+// Task #2
+
+// let  color = prompt("Enter a light");
+//
+// switch (color){
+//     case "green":
+//         alert("Go");
+//         break;
+//     case "yellow":
+//         alert("Ready");
+//         break;
+//     case "red":
+//         alert("Stay");
+//         break;
+//     default:
+//         alert("Do what you want!");
+//    }
+
+// Task #3
+
+let light = prompt("Enter light color");
+let isRoadClear = confirm("Is road clear?");
+
+if(light === "green" && isRoadClear ===true){
+    alert("Иди");
+} else  if (light === "green" && isRoadClear ===flase){
+    alert("Жди пока нарушитили проедут");
+}else  if (light === "yellow" && isRoadClear === true){
+    alert("Жди");
+}else  if (light === "yellow" && isRoadClear === false){
+    alert(" Всеравно жди");
+}else  if (light === "red" && isRoadClear === false){
+    alert(" Стой всеравно");
+}else  if (light === "red" && isRoadClear === true){
+    alert(" Стой  и  жди");
+}else{
+    alert("Делай что хочешь!");
+}
+
+
+
