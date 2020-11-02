071af458492322e29897580bc5a0b5b27a4d4d94
Kirill-Tretiakov <kirill.tretyakoff@gmail.com>
2020-11-02 16:51:23 +0200

    My 2 js homework

---
+++ homework2/script.js
@@ -0,0 +1,298 @@
+// Task #1
+
+/*
+let arr = [1,2,3,4,5];
+let arr1 = ['kirill', 'lera', 'alina', 'artem', 'marina'];
+let arr2 = ['kirill', 123, true, 'lera', 365, false];
+
+console.log(arr);
+console.log(arr1);
+console.log(arr2);*/
+
+
+// Task #2
+
+// let arr = [5];
+// arr [0] = 1;
+// arr [1] = 2;
+// arr [2] = 3;
+// arr [3] = 4;
+// arr [4] = 5;
+//
+// console.log(arr)
+
+// Task #4
+
+// for (let i =0; i<=10; i++){
+//     document.write('<div>Hello world</div>');
+// }
+
+// Task #5
+
+// for (let i =0; i<=10; i++){
+//     document.write('<div>Hello world</div>' + i);
+// }
+
+// Task #6
+
+// let i =0;
+//
+// while(i <=20){
+//     document.write('<h1>Hello world</h1>');
+// }
+
+// Task #7
+
+// let i =0;
+//
+// while(i<=10){
+//     document.write('<h1><Hello world/h1>' + i);
+// }
+
+// Task #8
+
+// let arr = [1,2,3,5,6,7,8,9,10]
+//
+// for(let i =0; i<10; i++){
+//     console.log(arr[i])
+// }
+
+// Task #9
+
+// let arr = ['kirill', 'lera', 'sasha', 'artem', 'marina', 'karina', 'liza', 'vova','nastya', 'roma']
+//
+// for (let i =0; i<10; i++){
+//     document.write(arr[i])
+// }
+
+
+// Task #10
+
+// let arr = ['kirill', true, 'artem', false, 'lera', true, 'liza', false, 'marina', false, 'vova', true]
+//
+// for (let i =0; i<arr.length; i++){
+//     if(typeof arr[i] === 'string'){
+//         console.log(arr[i])
+//     }
+// }
+
+// Task #11
+
+// let arr = ['kirill', true, 'artem', false, 'lera', true, 'liza', false, 'marina', false, 'vova', true]
+//
+// for (let i =0; i<arr.length; i++){
+//     if(typeof arr[i] === 'boolean'){
+//         console.log(arr[i])
+//     }
+// }
+
+// Task #12
+
+// let arr = ['kirill', 1, 'artem', 2, 'lera', 3, 'liza', 4, 'marina', 5, 'vova', 6]
+//
+// for (let i =0; i<arr.length; i++){
+//     if(typeof arr[i] === 'integer'){
+//         console.log(arr[i])
+//     }
+// }
+
+// Task #13
+
+// let arr = [];
+// arr[0] ='kirill';
+// arr[1]= 2;
+// arr[2]= true;
+// arr[3]= 3.14;
+// arr[4]= 'Vasya';
+// arr[5]= 5;
+// arr[6]= false;
+//
+// for (let i =0; i<arr.length; i++){
+//     console.log(arr[i])
+// }
+
+// Task #14
+
+// for (let i = 0; i<=10; i++){
+//     console.log(2020);
+//     document.write(2020);
+// }
+
+// Task #15
+
+// for (let i = 0; i<=100; i++){
+//     console.log(2020);
+//     document.write(2020);
+// }
+
+// Task #16
+
+// for (let i = 0; i<=100; i++){
+// if ((i % 2) === 0){
+//   console.log(i)
+// }
+// }
+
+// Task #17
+
+// for (let i =0; i<=100; i++){
+//         if (i%2){
+//             console.log(i)
+//         }
+// }
+
+// Task #18
+
+// for (let i =0; i<=2; i++){
+//     for (let k =0; k<=120; k++){
+//         console.log(i,k);
+//     }
+// }
+
+// Task #19
+
+// for (let i =0; i<=2; i++){
+//     for (let k =0; k<=120; k++){
+//         for (let j = 0; j<=7200; j++){
+//             console.log(i,k,j)
+//         }
+//     }
+// }
+
+// Task #20
+
+// let arr = ['a','b', 'c']
+// console.log(arr.join);
+
+//Task21
+
+// let arr = [1,2,3]
+// console.log(arr.reverse())
+
+//Task22
+//
+// let arr = [1,2,3];
+// arr.push(4,5,6);
+// console.log(arr);
+
+//Task23
+
+// let arr = [1,2,3];
+// arr.unshift(4,5,6);
+// console.log(arr);
+
+//Task24
+
+// let arr = ['js', 'css', 'jq']
+//
+// console.log(arr.shift())
+
+//Task25
+
+// let arr = ['js', 'css', 'jq']
+//
+// console.log(arr.pop())
+
+//Task26
+//
+// let arr = [1,2,3,4,5]
+// console.log(arr.slice(3,5));
+
+//Task27
+//
+// let arr = [1,2,3,4,5]
+// console.log(arr.slice(0,2));
+
+//Task26
+// let arr = [1,2,3,4,5]
+// arr.splice(1,2);
+// console.log(arr)
+
+//Task27
+// let arr = [1,2,3,4,5]
+// arr.splice(2, 0, "a", "b", "c");
+// console.log(arr);
+
+//Task28
+//
+// let arr = [1,2,3,4,5]
+// arr.splice(1, 0, "a","b");
+// arr.splice(6, 0, "c");
+// arr.splice(8, 0, "e");
+// console.log(arr);
+
+//Task29
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// for (let i = 0; i<=100; i++){
+// if ((arr[i] % 2) === 0){
+//    console.log(arr[i])
+//  }
+//  }
+
+//Task30
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// for (let i = 0; i<=100; i++){
+// if (arr[i] % 2){
+//    console.log(arr[i])
+//  }
+//  }
+
+//Task31
+
+// let arr = [1,2,3,4,];
+// let newArray=[]
+// for (let i =0; i<arr.length; i++){
+//     newArray.push(arr[i])
+// }
+// console.log(newArray)
+
+
+//Task32
+
+// let arr = [2,17,13,6,22,31,45,66,100, -18];
+//
+// let i =0;
+//
+// while(i<arr.length){
+//     console.log(arr[i])
+// }
+//
+// for (let k =0; k<arr.length; k++){
+//     console.log(arr[k])
+// }
+//
+// let j =0
+//
+// while(j<arr.length){
+//     if (arr[i]%2 === 0))
+//     console.log(arr[j])
+// }
+//
+// for(let c =0; c<arr.length; c++){
+//     if(arr[c]%2 === 0)
+//     console.log(arr[c])
+// }
+
+
+// console.log(arr.reverse())
+
+// Task #35
+
+// let arr = [];
+//
+// for (let i = 0; i <= 50; i++) {
+//     arr[i] = i ++;
+// }
+//
+// console.log(arr);
+
+// Task #36
+
+let arr = [];
+for ( i = 0; i < 20; i++ ) {
+    arr.push( Math.round( Math.random() * 100 ));
+}
+console.log(arr)
+
